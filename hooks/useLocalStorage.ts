4d68351c1526f8bd0052ae78f5d21a3aeaf8c8ce
Esan Samuel <esansamuel555@gmail.com>
2024-04-27 20:43:25 +0100

    updated useLocalStorage hook

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,15 +1,22 @@
+"use client";
 import React from "react";
 
 const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [T, (value: T) => void] => {
-  const storedValue = localStorage.getItem(key);
-  const initial = storedValue ? JSON.parse(storedValue) : initialValue;
-  const [value, setValue] = React.useState<T>(initial);
+  const [value, setValue] = React.useState<T>(() => {
+    if (typeof window !== "undefined") {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    }
+    return initialValue;
+  });
   const updateValue = (newValue: T) => {
     setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    }
   };
 
   return [value, updateValue];
