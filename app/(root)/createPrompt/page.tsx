632e1f06b0059fabea13a1837086c41ba15fa4b2
Esan Samuel <esansamuel555@gmail.com>
2024-05-12 19:37:52 +0100

    Update page.tsx

--- app/(root)/createPrompt/page.tsx
+++ app/(root)/createPrompt/page.tsx
@@ -164,8 +164,8 @@ const CreatePrompt = () => {
               ) : (
 <>
 {promptData && (
-<div className="w-full rounded-[10px] bg-dark-1 text-white flex gap-2 items-center p-2">
-<Image src={user?.image!} width={100} height={100} className="w-[12px] h-[12px] rounded-full" alt="You"/>
+<div className="w-full rounded-[10px] bg-dark-1 text-white flex gap-2 items-center p-2 mt-10">
+<Image src={user?.image!} width={100} height={100} className="w-[15px] h-[15px] rounded-full" alt="You"/>
 <>
 <h1 className="text-[13px]">{input}</h1>
 </>
